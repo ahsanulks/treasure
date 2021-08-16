3ad33a3e9298b266b8ea15e0ae22b828c36454b8
Ahsan <ahsanulkh996@gmail.com>
2021-08-16 11:05:36 +0700

    add player can found treasure 1 time

--- treasure.go
+++ treasure.go
@@ -43,14 +43,16 @@ var (
 			2: true,
 		},
 	}
-
-	arena [][]string
+	arena                    [][]string
+	probablyTreasurePosition [][]int
 )
 
 func main() {
-	setPlayerPosition(4, 1)
+	setStartPlayerPosition()
 	makeArena()
 	printArena()
+	searchTreasure()
+	fmt.Println(probablyTreasurePosition)
 }
 
 func makeArena() {
@@ -85,10 +87,87 @@ func isObstacle(y, x int) bool {
 	return y == 0 || y == 5 || x == 0 || x == 7 || obstaclePosition[y][x]
 }
 
+func setStartPlayerPosition() {
+	setPlayerPosition(4, 1)
+}
+
 func setPlayerPosition(y, x int) {
 	playerPosition = map[int]map[int]bool{
 		y: {
 			x: true,
 		},
 	}
+}
+
+/*
+	player only move with this following order
+	1. up
+	2. right
+	3. down
+*/
+func searchTreasure() {
+	var (
+		alreadyMoveUp    = false
+		alreadyMoveRight = false
+	)
+	for i := 0; i < 4; i++ {
+		if !alreadyMoveUp {
+			fmt.Println("player move up")
+			movePlayerUp()
+			alreadyMoveUp = true
+		} else if !alreadyMoveRight {
+			fmt.Println("player move right")
+			movePlayerRight()
+			if canMoveDown() {
+				alreadyMoveRight = true
+			}
+		} else {
+			fmt.Println("player move down")
+			movePlayerDown()
+			setProbablyTreasurePosition()
+		}
+		printArena()
+	}
+}
+
+func movePlayerUp() {
+	y, x := getCurrentPlayerPosition()
+	setPlayerPosition(y-1, x)
+	arena[y-1][x] = "X"
+	arena[y][x] = "."
+}
+
+func movePlayerRight() {
+	y, x := getCurrentPlayerPosition()
+	setPlayerPosition(y, x+1)
+	arena[y][x+1] = "X"
+	arena[y][x] = "."
+}
+
+func movePlayerDown() {
+	y, x := getCurrentPlayerPosition()
+	setPlayerPosition(y+1, x)
+	arena[y+1][x] = "X"
+	arena[y][x] = "."
+}
+
+func canMoveDown() bool {
+	y, x := getCurrentPlayerPosition()
+	return !isObstacle(y+1, x)
+}
+
+func getCurrentPlayerPosition() (y, x int) {
+	var currentYpositon, currentXpositon int
+	for y, xPosition := range playerPosition {
+		currentYpositon = y
+		for x := range xPosition {
+			currentXpositon = x
+		}
+	}
+	return currentYpositon, currentXpositon
+}
+
+func setProbablyTreasurePosition() {
+	y, x := getCurrentPlayerPosition()
+	probablyTreasurePosition = append(probablyTreasurePosition, []int{y, x})
 }
